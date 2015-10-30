a3837b7714f2b58210d3a59603f42eded25c2421
Sarah Graves <sjgraves142@gmail.com>
2015-10-30 13:10:51 -0400

    Add and execute function to calcualte GC content of DNA sequence in the house elf data

--- houseelf-analysis.r
+++ houseelf-analysis.r
@@ -1,6 +1,9 @@
 # Sarah Graves
 # House elf analysis
 
+# LOAD LIBRARIES
+library(stringr)
+
 # This script will analyze house elf data
 # Functions of analysis include:
   # classify based on ear length
@@ -10,3 +13,25 @@
 
 # import house elf data
 data <- read.csv("data/houseelf_earlength_dna_data_1.csv")
+
+
+# FUNCTION
+# calculate GC content of DNA sequence
+calculate_gc_pct <- function(seq_string){
+  # INPUT: string of a dna sequence
+  # OUTPUT: percent GC content for the string
+  
+  # convert the string to lower case
+  # if it is already lowercase, nothing will change
+  string_lower <- str_to_lower(seq_string)
+  
+  gs <- str_count(string_lower, 'g')
+  cs <- str_count(string_lower, 'c')
+  
+  # calcualte the percent
+  gc_content <- (gs + cs) / str_length(string_lower) * 100
+  
+  return(gc_content)}
+
+gc_pct <- calculate_gc_pct(data$dnaseq)
+
