db7c043ddf58b24a18c572a8ec1538fc6d7f68d3
Sarah Graves <sjgraves142@gmail.com>
2015-10-30 12:54:58 -0400

    Set up script for analysis

---
+++ houseelf-analysis.r
@@ -0,0 +1,8 @@
+# Sarah Graves
+# House elf analysis
+
+# This script will analyze house elf data
+# Functions of analysis include:
+  # classify based on ear length
+  # calculate the GC content
+  # output GC content for size classes
