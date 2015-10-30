9aa9cd7ef20b2f0281737bf5c6bb501f1b4780dc
Sarah Graves <sjgraves142@gmail.com>
2015-10-30 12:57:30 -0400

    Import house elf data file

--- houseelf-analysis.r
+++ houseelf-analysis.r
@@ -6,3 +6,7 @@
   # classify based on ear length
   # calculate the GC content
   # output GC content for size classes
+
+
+# import house elf data
+data <- read.csv("data/houseelf_earlength_dna_data.csv")
